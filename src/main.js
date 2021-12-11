35257743c0f3a46b381a66980e1b17a87c0df623
Ryan Cahela <6194542+RyanCahela@users.noreply.github.com>
2021-12-11 08:57:11 -0800

    added theme selector select form

---
+++ src/main.js
@@ -0,0 +1,27 @@
+(function main() {
+  //grab form elements
+  const themeSelector = document.querySelector(".js-color-input-form");
+  const body = document.body;
+
+  //grab array of all theme class names
+  const colorSelectOptionElements = themeSelector.querySelector(
+    ".js-color-scheme-select"
+  ).children;
+  const themeClassNames = Array.from(colorSelectOptionElements).map(
+    (element) => element.value
+  );
+
+  //listen for change
+  themeSelector.addEventListener("change", (e) => {
+    //on change remove all current theme classNames
+    for (let className of body.classList.values()) {
+      if (className === "body") {
+        continue;
+      }
+      body.classList.remove(className);
+    }
+
+    //apply value from select box
+    body.classList.add(e.target.value);
+  });
+})();
