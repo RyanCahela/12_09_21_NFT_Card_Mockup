5c1a6b4ce56c7e879d42c33cb4c359222fe7fbdb
Ryan Cahela <6194542+RyanCahela@users.noreply.github.com>
2021-12-13 15:44:56 -0800

    color preview component formats string on input event

--- src/create_new_color_scheme.js
+++
@@ -1,9 +0,0 @@
-(function create_new_color_scheme() {
-  const createNewSchemeForm = document.querySelector(
-    ".js-create-new-scheme-form"
-  );
-
-  createNewSchemeForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-  });
-})();
---
+++ src/js/create_new_color_scheme.js
@@ -0,0 +1,26 @@
+(function create_new_color_scheme() {
+  const createNewSchemeForm = document.querySelector(
+    ".js-create-new-scheme-form"
+  );
+
+  const primaryColorInput = document.querySelector("#js-primary-color");
+  const primaryColorPreview = document.querySelector(
+    "#js-primary-color-preview"
+  );
+
+  primaryColorInput.addEventListener("input", (e) => {
+    console.log("change", e.target.value);
+    const hexCode = appendWithHashtag(e.target.value);
+    primaryColorPreview.style.backgroundColor = hexCode;
+  });
+
+  createNewSchemeForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+
+  //helper function
+  function appendWithHashtag(string) {
+    if (string[0] === "#") return string;
+    return "#" + string;
+  }
+})();
